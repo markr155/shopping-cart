c453334707c7e943abdab32a5b4aab52ea14e559
Mark <markricho155@gmail.com>
2024-01-24 23:34:46 +1100

    Tests for product cards

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,32 +1,34 @@
 import { useState } from 'react';
 const ProductCard = ({ name, img, price, description }) => {
-	const [productNum, setProductNum] = useState(0);
+  const [productNum, setProductNum] = useState(0);
 
-	const incrementProductNum = (isAdding) => {
-		isAdding
-			? setProductNum((productNum) => productNum + 1)
-			: setProductNum((productNum) => productNum - 1);
-	};
+  const incrementProductNum = (isAdding) => {
+    isAdding
+      ? setProductNum((productNum) => productNum + 1)
+      : setProductNum((productNum) => {
+				if (productNum === 0) return productNum
+				return productNum - 1});
+  };
 
-	return (
-		<>
-			<div>
-				<img src={`${img}`} alt={`${name}`} />
-				<h3>{name}</h3>
-				<p>{price}</p>
-				<p>{description}</p>
-				<div>
-					<button onClick={() => incrementProductNum(false)}>-</button>
-					<input
-						type='number'
-						value={productNum}
-						onChange={(e) => setProductNum(parseInt(e.target.value))}
-					/>
-					<button onClick={() => incrementProductNum(true)}>+</button>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div>
+        <img src={`${img}`} alt={`${name}`} />
+        <h3>{name}</h3>
+        <p>{price}</p>
+        <p>{description}</p>
+        <div>
+          <button onClick={() => incrementProductNum(false)}>-</button>
+          <input
+            type='number'
+            value={productNum}
+            onChange={(e) => setProductNum(parseInt(e.target.value))}
+          />
+          <button onClick={() => incrementProductNum(true)}>+</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProductCard;
--- tests/ProductCard.test.jsx
+++ tests/ProductCard.test.jsx
@@ -1,37 +1,64 @@
 import { render, screen } from '@testing-library/react';
 import ProductCard from '../src/Components/ProductCard';
 import { beforeAll } from 'vitest';
+import userEvent from '@testing-library/user-event';
 
 describe('Product card renders each element', () => {
-	beforeEach(() => {
-		render(
-			<ProductCard
-				name={'name'}
-				img={'/'}
-				price={'100'}
-				description={'description'}
-			/>
-		);
-	});
-	it('Name of product rendered', () => {
-		expect(screen.queryByText('name')).toBeInTheDocument();
-	});
-	it('Image rendered', () => {
-		expect(screen.queryByAltText('name')).toBeInTheDocument();
-	});
-	it('Price rendered', () => {
-		expect(screen.queryByText('100')).toBeInTheDocument();
-	});
-	it('Description rendered', () => {
-		expect(screen.queryByText('description')).toBeInTheDocument();
-	});
-	it('Decrement product number button rendered', () => {
-		expect(screen.queryByText('-')).toBeInTheDocument();
-	});
-	it('Increment product number button rendered', () => {
-		expect(screen.queryByText('+')).toBeInTheDocument();
-	});
-	it('Number field rendered', () => {
-		expect(screen.queryByDisplayValue('0')).toBeInTheDocument();
-	})
+  beforeEach(() => {
+    render(
+      <ProductCard
+        name={'name'}
+        img={'/'}
+        price={'100'}
+        description={'description'}
+      />
+    );
+  });
+  it('Name of product rendered', () => {
+    expect(screen.queryByText('name')).toBeInTheDocument();
+  });
+  it('Image rendered', () => {
+    expect(screen.queryByAltText('name')).toBeInTheDocument();
+  });
+  it('Price rendered', () => {
+    expect(screen.queryByText('100')).toBeInTheDocument();
+  });
+  it('Description rendered', () => {
+    expect(screen.queryByText('description')).toBeInTheDocument();
+  });
+  it('Decrement product number button rendered', () => {
+    expect(screen.queryByText('-')).toBeInTheDocument();
+  });
+  it('Increment product number button rendered', () => {
+    expect(screen.queryByText('+')).toBeInTheDocument();
+  });
+  it('Number field rendered', () => {
+    expect(screen.queryByDisplayValue('0')).toBeInTheDocument();
+  });
+  it('Increment button increases number field by 1', async () => {
+    const user = userEvent.setup();
+    const inputField = screen.queryByDisplayValue('0');
+    const button = screen.getByRole('button', { name: '+' });
+
+    await user.click(button);
+    expect(inputField.value).toMatch('1');
+  });
+	it('Decrement button decreases number field by 1', async () => {
+    const user = userEvent.setup();
+    const inputField = screen.queryByDisplayValue('0');
+    const plusButton = screen.getByRole('button', { name: '+' });
+    const minusButton = screen.getByRole('button', { name: '-' });
+
+    await user.click(plusButton);
+		await user.click(minusButton);
+    expect(inputField.value).toMatch('0');
+  });
+	it('Decrement button doesnt go negative', async () => {
+    const user = userEvent.setup();
+    const inputField = screen.queryByDisplayValue('0');
+    const minusButton = screen.getByRole('button', { name: '-' });
+
+		await user.click(minusButton);
+    expect(inputField.value).toMatch('0');
+  });
 });
